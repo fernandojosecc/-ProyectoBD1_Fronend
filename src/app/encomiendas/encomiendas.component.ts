7e151694d4d7284e57b785bd451e086f0f9b982f
Pablin117 <tgelma117@gmail.com>
2022-10-27 14:24:10 -0600

    login

--- src/app/encomiendas/encomiendas.component.ts
+++ src/app/encomiendas/encomiendas.component.ts
@@ -11,6 +11,21 @@ export class EncomiendasComponent implements OnInit {
   encomiendas:any=[];
   encomiendas01:any=[];
   encomiendas02:any=[];
+  user:any={};
+  nameUser = ""
+  chats:any = []
+  allChats:any = []
+  value = 0
+  channel :any={}
+  newChannel :any={}
+  menus:any = []
+  menu1:boolean = false
+  menu2:boolean = false
+  menu3:boolean = false
+  menu4:boolean = false
+
+
+
   constructor(private http: HttpClient) { }
 
   ngOnInit(): void {
@@ -20,6 +35,30 @@ export class EncomiendasComponent implements OnInit {
     console.log(this.encomiendas01)
   }
   getChat(){
+
+    this.user = localStorage.getItem("user");
+    this.user = JSON.parse(this.user);
+    this.nameUser = this.user[0].user;
+
+    this.menus = this.user[0]
+    this.menus = this.menus[0].rolUsuarioMenuList
+    console.log(this.user)
+    console.log(this.menus)
+
+    /*if(){
+
+    }*/
+
+    for(let menu of this.menus){
+      console.log(menu)
+      switch (menu.idMenu){
+        case 1:this.menu1 =true; break;
+        case 2: this.menu2 =true; break
+        case 3: this.menu3 =true; break
+        case 4: this.menu4 =true; break
+      }
+    }
+
     this.findChat().subscribe(
       (respuesta:any) => this.formatChatOne(respuesta)
     )
--- src/app/misreservaciones/misreservaciones.component.ts
+++ src/app/misreservaciones/misreservaciones.component.ts
@@ -6,10 +6,44 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./misreservaciones.component.css']
 })
 export class MisreservacionesComponent implements OnInit {
+  user:any={};
+  nameUser = ""
+  chats:any = []
+  allChats:any = []
+  value = 0
+  channel :any={}
+  newChannel :any={}
+  menus:any = []
+  menu1:boolean = false
+  menu2:boolean = false
+  menu3:boolean = false
+  menu4:boolean = false
 
   constructor() { }
 
   ngOnInit(): void {
+    this.user = localStorage.getItem("user");
+    this.user = JSON.parse(this.user);
+    this.nameUser = this.user[0].user;
+
+    this.menus = this.user[0]
+    this.menus = this.menus[0].rolUsuarioMenuList
+    console.log(this.user)
+    console.log(this.menus)
+
+    /*if(){
+
+    }*/
+
+    for(let menu of this.menus){
+      console.log(menu)
+      switch (menu.idMenu){
+        case 1:this.menu1 =true; break;
+        case 2: this.menu2 =true; break
+        case 3: this.menu3 =true; break
+        case 4: this.menu4 =true; break
+      }
+    }
   }
 
 }
